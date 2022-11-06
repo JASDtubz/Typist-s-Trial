a42ba42dbadcdf15acf4be6a1faa13647f15758c
JASD <86506578+JASDtubz@users.noreply.github.com>
2022-11-05 21:40:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-/* Version 2022.11.05.19.03 */
+/* Version 2022.11.05.21.40 */
+
+var home = true;
 
 function setKeyDownListener() {
-  window.addEventListener("keydown", function(e) { keyDown(e.key); })
+  window.addEventListener("keydown", function(e) {
+    keyDown(e.key);
+  });
 }
 
 function keyDown(key) {
-  document.getElementById("input").textContent = document.getElementById("input").textContent + key;
+  if (home) {
+    document.getElementById("input").textConent = "";
+    home = false;
+  }
+  
+  if (key.length == 1) {
+    document.getElementById("input").textContent = document.getElementById("input").textContent + key;
+  }
 }
