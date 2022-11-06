9811bf796da8464620296117c88a7a11c5d12fa6
JASD <86506578+JASDtubz@users.noreply.github.com>
2022-11-06 14:07:57 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/* Version 2022.11.06.11.42 */
+/* Version 2022.11.06.14.07 */
 
 var home = true;
 var trial = null;
@@ -17,14 +17,18 @@ var prompts = [
 var sentance = null;
 var indexes = [];
 
-function setKeyDownListener() {
+function onLoad() {
   window.addEventListener("keydown", function(e) {
     keyDown(e.key);
+  });
+  window.addEventListner("pointerenter", function(e) {
+    
   });
 }
 
 function keyDown(key) {
   var input = document.getElementById("input");
+  var text = document.getElementById("text");
   
   if (key.length == 1) {
     if (home || input.textContent == " ") {
@@ -37,13 +41,14 @@ function keyDown(key) {
   }
   
   if (key == "." || key == "!" || key == "?") {
+    input.textContent = " ";
+    
     if (sentance == null && input.textContent == "Warm-up.") {
       let i = Math.floor(Math.random() * prompts[0].length);
       
       trial = false;
-      input.textContent = " ";
       sentance = prompts[0][i];
-      document.getElementById("text").textContent = sentance;
+      text.textContent = sentance;
       
       indexes.push(i);
     }
@@ -51,10 +56,39 @@ function keyDown(key) {
       trial = true;
     }
     else if (sentance != null && input.textContent == sentance) {
-      
+      if (indexes.length == 5) {
+        home = true;
+        sentance = null;
+        trial = null;
+        text.textContent = "Well done, you're ready for trial mode! Type 'Warm-up.' again to try for a better time or 'Trial.' for the real deal. Good luck!";
+      }
+      else {
+        if (trial) {
+          
+        }
+        else {
+          let b = true;
+          let i = 0;
+          
+          while (b) {
+            i = Math.floor(Math.random() * prompts[0].length);
+            b = false;
+            
+            for (int j = 0; j < indexes.length; j++) {
+              if (indexes[j] == i) {
+                b = true;
+              }
+            }
+          }
+          
+          sentance = prompts[0][i];
+          text.textContent = sentance;
+          
+          indexes.push(i);
+        }
+      }
     }
     else {
-      input.textContent = " ";
       input.style.backgroundColor = "#7f3f1f";
       input.style.color = "#7f3f1f";
       
