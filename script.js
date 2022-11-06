872a48a2bc90b2bc97c5c41c6ec71261c3450afa
JASD <86506578+JASDtubz@users.noreply.github.com>
2022-11-05 22:35:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/* Version 2022.11.05.22.06 */
+/* Version 2022.11.05.22.35 */
 
 var home = true;
 
@@ -13,7 +13,24 @@ function keyDown(key) {
     if (home) {
       document.getElementById("input").textContent = "";
       home = false;
-  }
+    }
+    
     document.getElementById("input").textContent = document.getElementById("input").textContent + key;
+  }
+  
+  if (key == "." || key == "!" || key == "?") {
+    if (document.getElementById("input").textContent == "Warm-up.") {
+      
+    }
+    else if (document.getElementById("input").textContent == "Trial.") {
+      
+    }
+    else {
+      document.getElementById("input").textContent = "";
+      document.getElementById("input").style.backgroundColor = "#fabeb4";
+      setTimeout(function() {
+        document.getElementById("input").style.backgroundColor = "#112233";
+      }, 500);
+    }
   }
 }
