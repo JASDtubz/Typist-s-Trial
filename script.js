c60ec2b88d1d272968396102194071151a69f2ca
JASD <86506578+JASDtubz@users.noreply.github.com>
2022-11-07 07:59:23 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/* Version 2022.11.06.15.12 */
+/* Version 2022.11.07.07.59 */
 
 var home = true;
 var trial = null;
@@ -60,48 +60,60 @@ function keyDown(key) {
       indexes.push(i);
       
       setTimeout(function() {
-        input.style.background = "#112233";
+        input.style.backgroundColor = "#112233";
       }, 500);
     }
     else if (sentance == null && typed == "Trial.") {
-      trial = true;
+      let i = Math.floor(Math.random() * prompts[1].length);
       
+      trial = true;
+      sentance = prompts[1][i];
+      text.textContent = sentance;
+      input.style.backgroundColor = "#7f7f1f";
+      
+      indexes.push(i);
+      
+      setTimeout(function() {
+        input.style.backgroundColor = "#112233";
+      }, 500);
     }
     else if (sentance != null && typed == sentance) {
       if (indexes.length == 5) {
         home = true;
         sentance = null;
         trial = null;
+        indexes = [];
         text.textContent = "Well done, you're ready for trial mode! Type 'Warm-up.' again to try for a better time or 'Trial.' for the real deal. Good luck!";
       }
       else {
-        if (trial) {
+        let ind = trial ? 1 : 0;
+        let b = true;
+        let i = 0;
+        
+        while (b) {
+          i = Math.floor(Math.random() * prompts[ind].length);
+          b = false;
           
-        }
-        else {
-          let b = true;
-          let i = 0;
-          
-          while (b) {
-            i = Math.floor(Math.random() * prompts[0].length);
-            b = false;
-            
-            for (let j = 0; j < indexes.length; j++) {
-              if (indexes[j] == i) {
-                b = true;
-              }
+          for (let j = 0; j < indexes.length; j++) {
+            if (indexes[j] == i) {
+              b = true;
             }
           }
-          
-          sentance = prompts[0][i];
-          text.textContent = sentance;
-          
-          indexes.push(i);
         }
+        
+        sentance = prompts[ind][i];
+        text.textContent = sentance;
+        input.style.backgroundColor = "#1f7f1f";
+        
+        indexes.push(i);
+        
+        setTimeout(function() {
+          input.style.backgroundColor = "#112233";
+        });
       }
     }
     else {
-      input.style.backgroundColor = "#7f3f1f";
+      input.style.backgroundColor = "#7f1f1f";
       
       setTimeout(function() {
         input.style.backgroundColor = "#112233";
