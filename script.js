bca6f116ce548cad62cbffe59f10670ca23015db
JASD <86506578+JASDtubz@users.noreply.github.com>
2022-11-07 08:10:56 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/* Version 2022.11.07.07.59 */
+/* Version 2022.11.07.08.10 */
 
 var home = true;
 var trial = null;
@@ -49,11 +49,12 @@ function keyDown(key) {
     
     input.textContent = " ";
     
-    if (sentance == null && typed == "Warm-up.") {
-      let i = Math.floor(Math.random() * prompts[0].length);
+    if (sentance == null && typed == "Warm-up." || typed == "Trial.") {
+      let ind = typed == "Trial" ? 1 : 0;
+      let i = Math.floor(Math.random() * prompts[ind].length);
       
       trial = false;
-      sentance = prompts[0][i];
+      sentance = prompts[ind][i];
       text.textContent = sentance;
       input.style.backgroundColor = "#7f7f1f";
       
@@ -62,30 +63,14 @@ function keyDown(key) {
       setTimeout(function() {
         input.style.backgroundColor = "#112233";
       }, 500);
-    }
-    else if (sentance == null && typed == "Trial.") {
-      let i = Math.floor(Math.random() * prompts[1].length);
-      
-      trial = true;
-      sentance = prompts[1][i];
-      text.textContent = sentance;
-      input.style.backgroundColor = "#7f7f1f";
-      
-      indexes.push(i);
-      
-      setTimeout(function() {
-        input.style.backgroundColor = "#112233";
-      }, 500);
-    }
-    else if (sentance != null && typed == sentance) {
+    } else if (sentance != null && typed == sentance) {
       if (indexes.length == 5) {
         home = true;
         sentance = null;
         trial = null;
         indexes = [];
         text.textContent = "Well done, you're ready for trial mode! Type 'Warm-up.' again to try for a better time or 'Trial.' for the real deal. Good luck!";
-      }
-      else {
+      } else {
         let ind = trial ? 1 : 0;
         let b = true;
         let i = 0;
@@ -109,10 +94,9 @@ function keyDown(key) {
         
         setTimeout(function() {
           input.style.backgroundColor = "#112233";
-        });
+        }, 500);
       }
-    }
-    else {
+    } else {
       input.style.backgroundColor = "#7f1f1f";
       
       setTimeout(function() {
